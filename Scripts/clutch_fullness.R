c8838ae6f18a33651356670463b773ea51137c7c
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2024-08-19 23:40:46 -0800

    Wohooo last indicator update for 2024

--- Scripts/clutch_fullness.R
+++ Scripts/clutch_fullness.R
@@ -1,12 +1,10 @@
-#Exploratory Plots
+#Calculate proportion full clutches in mature female snow crab
+
+#Author: Erin Fedewa
 
 library(tidyverse)
 library(ggridges)
 
-#2024 to do's:
-  #Write script for male and female size at maturity (re-create Jon's analysis)
-  #Revise female "proportion full" metric as 2024 indicator addition
-  #Calculate sex ratio as possible new indicator inclusion 
 
 ##############################################
 
@@ -17,23 +15,18 @@ sc_catch <- read.csv("./Data/crabhaul_opilio.csv")
 strata_sc <- read.csv("./Data/crabstrata_opilio.csv")
 
 ########################################
-#Proportion of mature females by clutch size (proxy for fecundity?)
+#Proportion of mature females by clutch size 
 sc_catch %>%
   mutate(YEAR = as.numeric(str_extract(CRUISE, "\\d{4}"))) %>%
-  filter(HAUL_TYPE != 17,
+  filter(HAUL_TYPE == 3,
          SEX == 2,
          CLUTCH_SIZE > 0,
          YEAR >= 1988) %>%
-  mutate(SHELL_CONDITION = case_when(SHELL_CONDITION == 2 ~ "SC2",
-                                     SHELL_CONDITION == 3 ~ "SC3",
-                                     SHELL_CONDITION == 4 ~ "SC4",
-                                     SHELL_CONDITION == 5 ~ "SC5"),
-         CLUTCH_TEXT = case_when(CLUTCH_SIZE == 1 ~ "Mature Barren",
-                                 CLUTCH_SIZE == 2 ~ "Trace",
-                                 CLUTCH_SIZE == 3 ~ "Quarter Full",
-                                 CLUTCH_SIZE == 4 ~ "Half Full",
-                                 CLUTCH_SIZE == 5 ~ "Three Quarter Full",
-                                 CLUTCH_SIZE == 6 ~ "Full")) -> female_ebs_spec
+  mutate(SHELL_CONDITION = case_when(SHELL_CONDITION %in% c(0,1,2) ~ "Primiparous",
+                                     SHELL_CONDITION > 2 ~ "Multiparous"),
+         CLUTCH_TEXT = case_when(CLUTCH_SIZE %in% c(1,2) ~ "Empty_trace",
+                                 CLUTCH_SIZE %in% c(3,4) ~ "Quarter_half",
+                                 CLUTCH_SIZE %in% c(5,6) ~ "Full")) -> female_ebs_spec
 #Compute CPUE
 female_ebs_spec %>%
   group_by(YEAR, GIS_STATION, AREA_SWEPT, SHELL_CONDITION, CLUTCH_TEXT) %>%
@@ -44,38 +37,27 @@ female_ebs_spec %>%
   #add in data field for total mature female population
   bind_rows(sc_catch %>% 
               mutate(YEAR = as.numeric(str_extract(CRUISE, "\\d{4}"))) %>%
-              filter(HAUL_TYPE == 3,SEX == 2,
+              filter(HAUL_TYPE == 3, SEX == 2,
                      CLUTCH_SIZE > 0,
                      YEAR >= 1988) %>% 
               mutate(CLUTCH_TEXT = "All",
-                     SHELL_CONDITION = case_when(SHELL_CONDITION == 2 ~ "SC2",
-                                                 SHELL_CONDITION == 3 ~ "SC3",
-                                                 SHELL_CONDITION == 4 ~ "SC4",
-                                                 SHELL_CONDITION == 5 ~ "SC5")) %>%
+                     SHELL_CONDITION = case_when(SHELL_CONDITION %in% c(0,1,2) ~ "Primiparous",
+                                                 SHELL_CONDITION > 2 ~ "Multiparous")) %>%
               group_by(YEAR, GIS_STATION, AREA_SWEPT, SHELL_CONDITION, CLUTCH_TEXT) %>%
               summarise(ncrab = round(sum(SAMPLING_FACTOR,na.rm = T)))) %>%
   filter(!is.na(CLUTCH_TEXT)) %>%
   ungroup() %>%
   mutate(cpue = ncrab / AREA_SWEPT) %>%
 #Join to stations that didn't catch crab 
-right_join(sc_catch %>% 
-               mutate(YEAR = as.numeric(str_extract(CRUISE, "\\d{4}"))) %>%
-               filter(HAUL_TYPE ==3,
-                      YEAR >= 1988) %>%
-               distinct(YEAR, GIS_STATION, AREA_SWEPT)) %>%
-               replace_na(list(cpue = 0)) -> catch
-  
-
-#join to stratum
-catch %>%
-  left_join(strata_sc %>%
-              select(STATION_ID, SURVEY_YEAR, STRATUM, TOTAL_AREA) %>%
-              filter(SURVEY_YEAR >= 1988) %>%
-              rename_all(~c("GIS_STATION", "YEAR",
-                            "STRATUM", "TOTAL_AREA"))) %>%
-  filter(SHELL_CONDITION != "NA",
-         CLUTCH_TEXT != "NA") %>%
-  #Scale to abundance by strata
+right_join(expand_grid(SHELL_CONDITION = c("Primiparous", "Multiparous"),
+                       CLUTCH_TEXT = c("Empty_trace", "Quarter_half", "Full", "All"),
+                       strata_sc %>%
+                         select(STATION_ID, SURVEY_YEAR, STRATUM, TOTAL_AREA) %>%
+                         filter(SURVEY_YEAR >= 1988) %>%
+                         rename_all(~c("GIS_STATION", "YEAR",
+                                       "STRATUM", "TOTAL_AREA")))) %>%
+               replace_na(list(ncrab = 0, cpue = 0)) %>%
+#Scale to abundance by strata
   group_by(YEAR, STRATUM, TOTAL_AREA, SHELL_CONDITION, CLUTCH_TEXT) %>%
   summarise(MEAN_CPUE = mean(cpue , na.rm = T),
             ABUNDANCE = (MEAN_CPUE * mean(TOTAL_AREA))) %>%
@@ -83,7 +65,7 @@ catch %>%
   #Sum across strata
   summarise(ABUNDANCE_MIL = sum(ABUNDANCE)/1e6) -> fem_abundance
 
-#Calculate proportion full  
+#Calculate proportion full of primiparous/multiparous females 
 fem_abundance %>%
   group_by(YEAR, SHELL_CONDITION) %>%
   summarise(Prop_full = (ABUNDANCE_MIL[CLUTCH_TEXT=="Full"]/ABUNDANCE_MIL[CLUTCH_TEXT=="All"])) -> full
@@ -93,7 +75,28 @@ full %>%
   ggplot() +
   geom_point(aes(x= YEAR, y=Prop_full)) + 
   geom_line(aes(x= YEAR, y=Prop_full)) +
-  facet_wrap(~SHELL_CONDITION)
+  facet_wrap(~SHELL_CONDITION) + 
+  theme_bw()
+
+#Plot primiparous females only
+full %>%
+  filter(SHELL_CONDITION == "Primiparous") %>%
+  ggplot() +
+  geom_point(aes(x= YEAR, y=Prop_full)) + 
+  geom_line(aes(x= YEAR, y=Prop_full)) +
+  geom_hline(aes(yintercept = mean(Prop_full, na.rm=TRUE)), linetype = 5) +
+  theme_bw()
+
+#write csv for indicator 
+missing <- data.frame(YEAR = 2020)
+
+full %>%
+  filter(SHELL_CONDITION == "Primiparous") %>%
+  select(-SHELL_CONDITION) %>%
+  bind_rows(missing) %>%
+  arrange(YEAR) %>%
+  write.csv(file="./Output/clutch_full.csv")
+  
 
 
 
