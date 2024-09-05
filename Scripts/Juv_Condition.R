4fa18c103b5a08098a784af034529bc08397c868
Erin-Fedewa-NOAA <erin.fedewa>
2024-09-05 12:21:48 -0800

    Updating energetic condition plots

--- Scripts/Juv_Condition.R
+++ Scripts/Juv_Condition.R
@@ -7,6 +7,9 @@
 # load ----
 library(tidyverse)
 library(ggridges)
+library(akgfmaps)
+library(patchwork)
+library(viridis)
 
 #######################################
 
@@ -29,7 +32,8 @@ cond %>%
   arrange(year) %>%
 write.csv(file="./Output/opilio_condition.csv")
 
-#Plot 
+#####################################################
+#Plots 
 sc_condition %>%
   filter(lme != "NA", #one crab collected outside the sampling design
          lme != "NBS",
@@ -37,24 +41,84 @@ sc_condition %>%
          maturity != 1) %>%
   group_by(year) %>%
   summarise(sd = sd(Perc_DWT, na.rm = TRUE),
-    cond = mean(Perc_DWT, na.rm=T)) -> plot
-    
+    cond = mean(Perc_DWT, na.rm=T)) %>%
+  bind_rows(missing) %>%
+  arrange(year) -> plot
+
+#plot annual mean as bar plot    
 plot %>%
   ggplot(aes(as.factor(year), cond, fill=year)) +
   geom_bar(stat="identity") + 
   geom_errorbar(aes(ymin = cond-sd, ymax = cond+sd), width = 0.2, color="gray45") +
   theme_classic() +
-  labs(y = "Snow Crab Condition", x = "") +
+  labs(y = "Snow Crab Condition (% DWT)", x = "") +
   theme(axis.text=element_text(size=12)) +
-  theme(legend.position = "none")
+  theme(legend.position = "none") +
+  geom_hline(yintercept=24, color="red") #adding prelim threshold from starvation lab experiment
   
-
+#plot annual mean as dot plot
 plot %>%
-  ggplot(aes(as.factor(year), cond, fill=year)) +
-  geom_point() + 
+  ggplot(aes(as.factor(year), cond)) +
+  geom_point(size=2) + 
   labs(y = "Snow Crab Condition", x = "") +
   theme(axis.text=element_text(size=12)) +
   theme(legend.position = "none") +
-  geom_hline(aes(yintercept = mean(cond, na.rm=TRUE)), linetype = 5) +
+  geom_hline(aes(yintercept = mean(cond, na.rm=TRUE)), linetype = 5)+
+  geom_hline(aes(yintercept = quantile(cond, .10, na.rm=TRUE)), linetype = 3)+
+  geom_hline(aes(yintercept = quantile(cond, .90, na.rm=TRUE)), linetype = 3)+
   theme_bw()
+
+#size comp of crab sampled
+sc_condition %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         lme != "NBS",
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66"),
+         maturity != 1) %>%
+  mutate(Sex = recode_factor(sex, '1' = "M", '2' = "F")) %>%
+  filter(lme != "NA", sex != "NA") %>% #one crab collected outside the sampling design
+  group_by(year) %>%
+  ggplot() +
+  geom_density(aes(x=cw, fill=Sex), position = "stack", binwidth = 2) +
+  scale_fill_manual(values=c("#00BFC4", "#F8766D")) +
+  facet_grid(lme~year) +
+  theme_bw() +
+  labs(x= "Snow crab carapace width (mm)", y = "Count")
+
+#Spatial condition map 
+
+# Plotting layers
+ebs_layers <- akgfmaps::get_base_layers(select.region = "ebs", set.crs = "EPSG:3338")
+
+ebs_survey_areas <- ebs_layers$survey.area
+
+ebs_survey_areas$survey_name <- c("Eastern Bering Sea", "Northern Bering Sea")
+
+#Transform crab data 
+sc_condition %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66"),
+         maturity != 1) %>%
+  group_by(year, mid_latitude, mid_longitude) %>%
+  summarise(condition=mean(Perc_DWT)) %>%
+  # Convert lat/long to an sf object
+  st_as_sf(coords = c("mid_longitude", "mid_latitude"), crs = st_crs(4326)) %>%
+  #st_as_sf needs crs of the original coordinates- need to transform to Alaska Albers
+  st_transform(crs = st_crs(3338)) -> crab_dat
+
+#And now plot survey grid with crab data  
+ggplot() +
+  geom_sf(data = ebs_layers$survey.grid, fill=NA, color=alpha("grey80"))+
+  geom_sf(data = ebs_survey_areas, fill = NA) +
+  geom_sf(data = ebs_layers$akland, fill = "grey80", color = "black") +
+  #add crab layers
+  geom_sf(data=crab_dat, aes(color=condition)) +
+  scale_color_viridis() +
+  scale_x_continuous(limits = ebs_layers$plot.boundary$x,
+                     breaks = ebs_layers$lon.breaks) +
+  scale_y_continuous(limits = ebs_layers$plot.boundary$y,
+                     breaks = ebs_layers$lat.breaks) +
+  #scale_size_continuous(range = c(1,4)) +
+  labs(x="", y="", color = expression(paste("% DWT"))) +
+  theme_bw() +
+  facet_wrap(~year) 
 
